29e8d3c10cafaa72baf4ada9ce21a124aa7f688c
Roxanne Lutz <rlutz1@unm,edu>
2025-03-31 08:23:04 -0600

    pushing, need to refactor ms to not use temp array. run out of memory when 2^30 run. kinda figured that would be an issue.

--- src/Driver.java
+++ src/Driver.java
@@ -2,8 +2,24 @@
 
 public class Driver {
 
+    public static final String FILE_NAME = "Integer_Unsorted_2^30"; // playground
+    public static final int NUM_NUMBERS = (int) Math.pow(2, 30); // 16
+
     public static void main(String[] args) {
-        TestCaseGenerator gtc = new TestCaseGenerator();
+        ThreeWayMergeSort ms = new ThreeWayMergeSort();
+        ms.initArray(
+                "C:\\Users\\lutzr\\CS351\\ProjectSource\\CS361_Project\\tests\\" + FILE_NAME,
+                NUM_NUMBERS
+        );
+        ms.print(false);
+        ms.sort(0, NUM_NUMBERS - 1);
+        System.out.println("Done.");
+        System.out.println("Sorted: " + ms.isSorted());
+
+    }
+}
+
+//TestCaseGenerator gtc = new TestCaseGenerator();
 //        gtc.generate((int) Math.pow(2, 20), -500, 500, false, "Integer_Unsorted_2^20");
 //        gtc.generate((int) Math.pow(2, 21), -500, 500, false, "Integer_Unsorted_2^21");
 //        gtc.generate((int) Math.pow(2, 22), -500, 500, false, "Integer_Unsorted_2^22");
@@ -15,7 +31,5 @@ public class Driver {
 //        gtc.generate((int) Math.pow(2, 28), -500, 500, false, "Integer_Unsorted_2^28");
 //        gtc.generate((int) Math.pow(2, 29), -500, 500, false, "Integer_Unsorted_2^29");
 
-        // TODO: short[] fixed the issue, maybe note with memory issue discussion
+// TODO: short[] fixed the issue, maybe note with memory issue discussion
 //        gtc.generate((int) Math.pow(2, 30), -500, 500, false, "Integer_Unsorted_2^30");
-    }
-}
---
+++ src/ThreeWayMergeSort.java
@@ -0,0 +1,169 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * todo:
+ * -- read in text file to formulate a short[]; make this easy to maybe abstract out
+ * --
+ */
+
+public class ThreeWayMergeSort {
+
+    /*
+     * =============================================================
+     * GLOBALS/CONSTANTS
+     * =============================================================
+     */
+
+    private short[] toSort = null;
+
+
+    /*
+     * =============================================================
+     * CONSTRUCTORS
+     * =============================================================
+     */
+
+    public ThreeWayMergeSort() { } // end constructor
+
+
+    /*
+     * =============================================================
+     * HELPER METHODS
+     * =============================================================
+     */
+
+
+    public void sort(int low, int high) {
+//        System.out.println("low " + low + " high " + high);
+        if (low >= high) { return; }
+        if (high - low == 1) { swapIfNeeded(low, high); return;}
+
+        int third = (high - low) / 3;
+
+        sort (low, low + third);
+        sort (low + third + 1, high - third);
+        sort (high - third + 1, high);
+
+        copyIn (
+                merge (low, low + third, low + third + 1, high - third),
+                low,
+                high - third
+        );
+        copyIn (
+                merge (low, high - third, high - third + 1, high),
+                low,
+                high
+        );
+//        print(true);
+    }
+
+    private short[] merge(int startA, int endA, int startB, int endB) {
+        int i = startA, j = startB, k = 0;
+        short[] temp = new short[endB - startA + 1];
+
+
+        while (i <= endA && j <= endB) {
+//            System.out.println("while1");
+            if (toSort[i] > toSort[j]) {
+                temp[k] = toSort[j]; k++; j++;
+            } else {
+                temp[k] = toSort[i]; k++; i++;
+            } // end if
+        } // end loop
+
+        while (i <= endA) {
+//            System.out.println("while2");
+            temp[k] = toSort[i]; k++; i++;
+        } // end while
+
+        while (j <= endB) {
+//            System.out.println("while3");
+            temp[k] = toSort[j]; k++; j++;
+        } // end while
+
+        return temp;
+
+    } // end method
+
+    private void copyIn(short[] temp, int start, int end) {
+        for (int i = start, j = 0; i <= end; i++, j++) {
+            toSort[i] = temp[j];
+        } // end loop
+    } // end method
+
+    private void swapIfNeeded(int low, int high) {
+        if (toSort[low] > toSort[high])  {
+            swap(low, high);
+        } // end if
+    } // end method
+
+
+    private void swap(int x, int y) {
+        short temp = toSort[x];
+        toSort[x] = toSort[y];
+        toSort[y] = temp;
+    } // end method
+
+    /**
+     * method common to all sort classes to read in a test case file
+     * from basic java io operations.
+     * @param path file path given, will be ultimately from the command line/from driver
+     *             note that the test cases are all given as csv format
+     */
+    public void initArray(String path, int howMany) {
+        toSort = new short[howMany]; int counter = 0;
+        try {
+            Scanner s = new Scanner(new File(path)).useDelimiter(",");
+            while (s.hasNext()) {
+                toSort[counter] = s.nextShort(); counter++;
+            } // end loop
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Oops, no file there. Love, mergesort.");
+        } // end try/catch
+
+    } // end method
+
+    /**
+     * simple print function for testing purposes.
+     */
+    public void print(boolean printNums) {
+        if (printNums) {
+            for (int num : toSort) {
+                System.out.print(num + " ");
+            } // end loop
+        } // end if
+
+        System.out.println("Found " + toSort.length + " numbers.");
+    } // end method
+
+    /**
+     * simple ascending list checker to run for testing purposes on
+     * large data sets. to make my life eaiser :)
+     * @return if the list has been sorted
+     */
+    public boolean isSorted() {
+        int i = 0, j = 1;
+
+        while (j < toSort.length) {
+            if (toSort[i] > toSort[j]) {
+                return false;
+            } // end if
+            i++; j++;
+        } // end loop
+
+        return true;
+    } // end method
+
+    /*
+     * =============================================================
+     * GETTERS/SETTERS
+     * =============================================================
+     */
+
+    public short[] getToSort() {
+        return toSort;
+    } // end getter
+
+}
